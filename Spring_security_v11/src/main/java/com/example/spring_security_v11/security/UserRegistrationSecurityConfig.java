72984c74d37a4b05d674ae532d9dc06aa060186a
sirpolux <poluxreloaded@gmail.com>
2023-09-05 22:42:59 +0100

    feat-UserSecurityConfiguration

--- Spring_security_v11/src/main/java/com/example/spring_security_v11/security/UserRegistrationSecurityConfig.java
+++ Spring_security_v11/src/main/java/com/example/spring_security_v11/security/UserRegistrationSecurityConfig.java
@@ -2,9 +2,11 @@ package com.example.spring_security_v11.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -13,4 +15,12 @@ public class UserRegistrationSecurityConfig {
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
+
+//    public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
+//        return http.cors()
+//                .and().csrf().disable()
+//                .authorizeHttpRequests().requestMatchers()
+//
+//
+//    }
 }
